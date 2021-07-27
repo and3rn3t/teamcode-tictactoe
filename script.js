a4fdc647eb3067b325a77d95447b87536f083382
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-07-27 13:52:25 -0500

    added logic to determine winner

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ let squares = document.querySelectorAll('.square');
 //for now will go with player 1, or red goes first
 let nextPlayer = 'red';
 let refresh = document.querySelector('#reset');
-
+let count = 1;
+document.querySelector('#whosturn').innerHTML = `it's ${nextPlayer}'s turn`;
 function buttonClicked(evt) {
     evt.preventDefault();
-    console.log("in buttonclicked");
-    console.log(evt);
-    console.log(evt.srcElement);
-    console.log("color is",evt.srcElement.style.background_color);
+ //   console.log("in buttonclicked",count);
+//  console.log(evt);
+//    console.log(evt.srcElement);
+//    console.log("color is",evt.srcElement.style.background);
     //if square already picked, alert to choose another square
     if (evt.srcElement.style.background == 'red' || evt.srcElement.style.background == 'blue') {
         alert('This square already picked, choose another');
@@ -20,23 +21,54 @@ function buttonClicked(evt) {
     // change square[i] to color of nextPlayer - //squares[].style.background-color = nextPlayer;
     evt.srcElement.style.background = nextPlayer;
     // change nextPlayer color
+    checkForWinner(nextPlayer,count);
     if (nextPlayer == 'red') {
         nextPlayer = 'blue';
     } else {
         nextPlayer = 'red';
     }
+    document.querySelector('#whosturn').innerHTML = `it's ${nextPlayer}'s turn`;
+    count++;
 }
 
 function resetBoard(evt) {
+    count = 1;
     evt.preventDefault();
     for (let j = 0; j < squares.length; j++) {
         squares[j].style.background = 'bisque';
     }
 }
 
+function checkForWinner(winColor,inCount) {
+//   let possibleWInners = [squares[0],squares[1],squares[2]], [sqaures[3],squares[4],squares[5]];
+//console.log('in check winner',winColor,count);
+//console.log('0 = ',squares[0].style.background);
+    if (squares[0].style.background == winColor && squares[1].style.background == winColor && squares[2].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[3].style.background == winColor && squares[4].style.background == winColor && squares[5].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[6].style.background == winColor && squares[7].style.background == winColor && squares[8].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[0].style.background == winColor && squares[3].style.background == winColor && squares[6].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[1].style.background == winColor && squares[4].style.background == winColor && squares[7].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[2].style.background == winColor && squares[5].style.background == winColor && squares[8].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else if (squares[0].style.background == winColor && squares[4].style.background == winColor && squares[8].style.background == winColor) {
+        alert(`${winColor} is the winner`);
+    } else if (squares[2].style.background == winColor && squares[4].style.background == winColor && squares[6].style.background == winColor) {
+        alert(`${winColor} is the winner!`);
+    } else {
+        if (inCount == 9) {
+        alert('This game is a draw..., try again?')
+      }
+    }
+}
+
     //create a loop for the event listener
     for (let i = 0; i < squares.length; i++) {
-        console.log(i);
+  //      console.log(i);
         squares[i].addEventListener('click', buttonClicked);
     }
 
