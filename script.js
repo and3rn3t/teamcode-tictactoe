5731dabeef95e7a2d9a398490c19def7123a4c17
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-07-27 10:27:41 -0500

    initial js.script code. Event Listener added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//query selector to get each tic tac toe square
+//query selector to get the tic tac toe squares
+let squares = document.querySelectorAll('.tictactoeboard');
+//let Player1 = 'red';
+//let Player2 = 'blue';
+//for now will go with player 1, or red goes first
+let nextPlayer = 'red';
+
+function ButtonClicked () {
+  // if square[i] not white, alert or message pick another square, return
+  // change square[i] to color of nextPlayer - //squares[].style.background-color = nextPlayer;
+  // change nextPlayer color
+}
+
+//create a loop for the event listener
+for (let i = 0; i < squares.length;i++) {
+    squares[i].addEventListener('click',ButtonClicked);
+}
