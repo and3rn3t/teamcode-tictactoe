de2bc16fb3806c91848a6a37012fa08b9ee031f7
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-07-27 11:33:01 -0500

    Working tic tac toe

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 //query selector to get the tic tac toe squares
-let squares = document.querySelectorAll('.tictactoeboard');
+let squares = document.querySelectorAll('.square');
 //let Player1 = 'red';
 //let Player2 = 'blue';
 //for now will go with player 1, or red goes first
 let nextPlayer = 'red';
+let refresh = document.querySelector('#reset');
 
-function ButtonClicked () {
-  // if square[i] not white, alert or message pick another square, return
-  // change square[i] to color of nextPlayer - //squares[].style.background-color = nextPlayer;
-  // change nextPlayer color
+function buttonClicked(evt) {
+    evt.preventDefault();
+    console.log("in buttonclicked");
+    console.log(evt);
+    console.log(evt.srcElement);
+    console.log("color is",evt.srcElement.style.background_color);
+    //if square already picked, alert to choose another square
+    if (evt.srcElement.style.background == 'red' || evt.srcElement.style.background == 'blue') {
+        alert('This square already picked, choose another');
+        return;
+    }
+    // change square[i] to color of nextPlayer - //squares[].style.background-color = nextPlayer;
+    evt.srcElement.style.background = nextPlayer;
+    // change nextPlayer color
+    if (nextPlayer == 'red') {
+        nextPlayer = 'blue';
+    } else {
+        nextPlayer = 'red';
+    }
 }
 
-//create a loop for the event listener
-for (let i = 0; i < squares.length;i++) {
-    squares[i].addEventListener('click',ButtonClicked);
+function resetBoard(evt) {
+    evt.preventDefault();
+    for (let j = 0; j < squares.length; j++) {
+        squares[j].style.background = 'bisque';
+    }
 }
+
+    //create a loop for the event listener
+    for (let i = 0; i < squares.length; i++) {
+        console.log(i);
+        squares[i].addEventListener('click', buttonClicked);
+    }
+
+    reset.addEventListener('click', resetBoard);
