f21bb0a7b46370310a751c1a1f851f0ee08479b3
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-07-27 14:00:38 -0500

    Working tic tac toe game

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ let count = 1;
 document.querySelector('#whosturn').innerHTML = `it's ${nextPlayer}'s turn`;
 function buttonClicked(evt) {
     evt.preventDefault();
- //   console.log("in buttonclicked",count);
-//  console.log(evt);
-//    console.log(evt.srcElement);
-//    console.log("color is",evt.srcElement.style.background);
+    //   console.log("in buttonclicked",count);
+    //  console.log(evt);
+    //    console.log(evt.srcElement);
+    //    console.log("color is",evt.srcElement.style.background);
     //if square already picked, alert to choose another square
     if (evt.srcElement.style.background == 'red' || evt.srcElement.style.background == 'blue') {
         alert('This square already picked, choose another');
@@ -21,7 +21,7 @@ function buttonClicked(evt) {
     // change square[i] to color of nextPlayer - //squares[].style.background-color = nextPlayer;
     evt.srcElement.style.background = nextPlayer;
     // change nextPlayer color
-    checkForWinner(nextPlayer,count);
+    checkForWinner(nextPlayer, count);
     if (nextPlayer == 'red') {
         nextPlayer = 'blue';
     } else {
@@ -34,42 +34,43 @@ function buttonClicked(evt) {
 function resetBoard(evt) {
     count = 1;
     evt.preventDefault();
+    document.querySelector('#winner').innerHTML = '';
     for (let j = 0; j < squares.length; j++) {
         squares[j].style.background = 'bisque';
     }
 }
 
-function checkForWinner(winColor,inCount) {
-//   let possibleWInners = [squares[0],squares[1],squares[2]], [sqaures[3],squares[4],squares[5]];
-//console.log('in check winner',winColor,count);
-//console.log('0 = ',squares[0].style.background);
+function checkForWinner(winColor, inCount) {
+    //   let possibleWInners = [squares[0],squares[1],squares[2]], [sqaures[3],squares[4],squares[5]];
+    //console.log('in check winner',winColor,count);
+    //console.log('0 = ',squares[0].style.background);
     if (squares[0].style.background == winColor && squares[1].style.background == winColor && squares[2].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[3].style.background == winColor && squares[4].style.background == winColor && squares[5].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[6].style.background == winColor && squares[7].style.background == winColor && squares[8].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[0].style.background == winColor && squares[3].style.background == winColor && squares[6].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[1].style.background == winColor && squares[4].style.background == winColor && squares[7].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[2].style.background == winColor && squares[5].style.background == winColor && squares[8].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[0].style.background == winColor && squares[4].style.background == winColor && squares[8].style.background == winColor) {
-        alert(`${winColor} is the winner`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else if (squares[2].style.background == winColor && squares[4].style.background == winColor && squares[6].style.background == winColor) {
-        alert(`${winColor} is the winner!`);
+        document.querySelector('#winner').innerHTML = `${winColor} is the winner!`;
     } else {
         if (inCount == 9) {
-        alert('This game is a draw..., try again?')
-      }
+            document.querySelector('#winner').innerHTML = 'This game is a draw..., try again?';
+        }
     }
 }
 
-    //create a loop for the event listener
-    for (let i = 0; i < squares.length; i++) {
-  //      console.log(i);
-        squares[i].addEventListener('click', buttonClicked);
-    }
+//create a loop for the event listener
+for (let i = 0; i < squares.length; i++) {
+    //      console.log(i);
+    squares[i].addEventListener('click', buttonClicked);
+}
 
-    reset.addEventListener('click', resetBoard);
+reset.addEventListener('click', resetBoard);
